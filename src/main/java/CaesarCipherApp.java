75479be923b31bd716972a9929e406dc99ce2806
rodgersmm <rodgersmutua44@gmail.com>
2023-07-05 15:00:36 +0300

    Define encoding and input strings

--- src/main/java/CaesarCipherApp.java
+++ src/main/java/CaesarCipherApp.java
@@ -1,2 +1,27 @@
+
+
+
+
 public class CaesarCipherApp {
+    //String to hold alphabets
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encryptData(String inStr, int shiftKey)
+    {
+
+
+        //String to store encrypted data
+        String encString = "";
+
+        //Convert entered string to lowercase
+        inStr = inStr.toLowerCase();
+
+        for (int i = 0;i<inStr.length();i++)
+        {
+
+
+        }
+
+
+    }
 }
